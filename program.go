0ad809361db178edcb02f3b39a83b315940c9a9f
anantjainaccolite <98074007+anantjainaccolite@users.noreply.github.com>
2022-06-01 18:37:06 +0530

    Update program.go

--- program.go
+++ program.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 //Consumer
 type Consumer struct {
-	items *chan int
+	items     *chan int
 	timestamp *chan string
 }
 
 //creating a new Consumer
-func NewConsumer(items *chan int) *Consumer {
+func NewConsumer(items *chan int, timestamp *chan string) *Consumer {
 	return &Consumer{items: items, timestamp: timestamp}
 }
 
@@ -19,16 +20,21 @@ func NewConsumer(items *chan int) *Consumer {
 func (c *Consumer) consume() {
 	fmt.Println("consume: Started")
 	for {
+		// time.Sleep(45 * time.Millisecond)
+		if len(*c.items) <= 0 {
+			continue
+		}
+		fmt.Println("consuming...")
 		item := <-*c.items
 		timestamps := <-*c.timestamp
-		fmt.Println("Received:", item, " which was produced at: ",timestamps)
+		fmt.Println("Received:", item, " which was produced at: ", timestamps)
 	}
 }
 
 //Producer
 type Producer struct {
-	items *chan int
-	done  *chan bool
+	items     *chan int
+	done      *chan bool
 	timestamp *chan string
 }
 
@@ -39,28 +45,42 @@ func NewProducer(items *chan int, done *chan bool, timestamp *chan string) *Prod
 
 //producing and sending the item through the channel
 func (p *Producer) produce(max int) {
+	i := 1
 	fmt.Println("produce: Started")
-	for i := 0; i < max; i++ {
-		fmt.Println("produce: Sending ", i)
+	for {
+
+		if len(*p.items) >= max {
+			continue
+		}
+		fmt.Println("starting.....")
 		*p.items <- i
-		*p.timestamp <- string(time.crrentTime)
+		fmt.Println("starting 1.....")
+		*p.timestamp <- time.Now().String()
+		fmt.Println("starting 2.....")
+		fmt.Println("produce: Sending ", i)
+		i++
+		// time.Sleep(30 * time.Millisecond)
 	}
-	*p.done <- true // signal when done
-	fmt.Println("produce: Done")
+	// *p.done <- true // signal when done
+	// fmt.Println("produce: Done")
 }
 
 func main() {
 
-	var items = make(chan int) // channel to send items
-	var done = make(chan bool) // channel to control when production is done
-	var timestamp = make(chan string)
-	// Start a goroutine for Produce.produce
-	go NewProducer(&items, &done, &timestamp).produce(5)
+	var items = make(chan int)        // channel to send items
+	var done = make(chan bool)        // channel to control when production is done
+	var timestamp = make(chan string) //channel to  add timestamps
 
+	// Start a goroutine for Produce.produce
+	len := 5
+	go NewProducer(&items, &done, &timestamp).produce(len)
+	fmt.Println("in betwenn")
 	// Start a goroutine for Consumer.consume
 	go NewConsumer(&items, &timestamp).consume()
 
+	time.Sleep(100 * time.Second)
 	// Finish the program when the production is done
-	<-done
+	fmt.Println("last")
+	// <-done
 
 }
