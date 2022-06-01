c41028e286be1546a102c6e1677973d2a6e3c017
SukratiPandit <106656042+SukratiPandit@users.noreply.github.com>
2022-06-01 17:56:58 +0530

    Update program.go

--- program.go
+++ program.go
@@ -7,11 +7,12 @@ import (
 //Consumer
 type Consumer struct {
 	items *chan int
+	timestamp *chan string
 }
 
 //creating a new Consumer
 func NewConsumer(items *chan int) *Consumer {
-	return &Consumer{items: items}
+	return &Consumer{items: items, timestamp: timestamp}
 }
 
 //consuming items from the channel
@@ -19,7 +20,8 @@ func (c *Consumer) consume() {
 	fmt.Println("consume: Started")
 	for {
 		item := <-*c.items
-		fmt.Println("consume: Received:", item)
+		timestamps := <-*c.timestamp
+		fmt.Println("Received:", item, " which was produced at: ",timestamps)
 	}
 }
 
@@ -27,11 +29,12 @@ func (c *Consumer) consume() {
 type Producer struct {
 	items *chan int
 	done  *chan bool
+	timestamp *chan string
 }
 
 //creating a new Producer
-func NewProducer(items *chan int, done *chan bool) *Producer {
-	return &Producer{items: items, done: done}
+func NewProducer(items *chan int, done *chan bool, timestamp *chan string) *Producer {
+	return &Producer{items: items, done: done, timestamp: timestamp}
 }
 
 //producing and sending the item through the channel
@@ -40,6 +43,7 @@ func (p *Producer) produce(max int) {
 	for i := 0; i < max; i++ {
 		fmt.Println("produce: Sending ", i)
 		*p.items <- i
+		*p.timestamp <- string(time.crrentTime)
 	}
 	*p.done <- true // signal when done
 	fmt.Println("produce: Done")
@@ -49,12 +53,12 @@ func main() {
 
 	var items = make(chan int) // channel to send items
 	var done = make(chan bool) // channel to control when production is done
-
+	var timestamp = make(chan string)
 	// Start a goroutine for Produce.produce
-	go NewProducer(&items, &done).produce(5)
+	go NewProducer(&items, &done, &timestamp).produce(5)
 
 	// Start a goroutine for Consumer.consume
-	go NewConsumer(&items).consume()
+	go NewConsumer(&items, &timestamp).consume()
 
 	// Finish the program when the production is done
 	<-done
